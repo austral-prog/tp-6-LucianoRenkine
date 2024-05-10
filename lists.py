46fa02cca67ad1c147d5406baaef5be9280263f1
LucianoRenkine <162723184+LucianoRenkine@users.noreply.github.com>
2024-05-10 13:05:32 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,40 @@
-# Replace the "ANSWER HERE" with your answer
+def remove_elements(lista):
+	if len(lista) >= 6:
+		del lista[5]
+		del lista[4]
+		del lista[0]
+		return lista
+	elif len(lista) == 5:
+		del lista[4]
+		del lista[0]
+		return lista
+	else:
+		del lista[0]
+		return lista
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(lista_2):
+	lista_2.append("Yellow")
+	lista_2.insert(0,"Pink")
+	return lista_2
 
+def is_empty(lista_3):
+	if len(lista_3) == 0:
+		return "Esta lista está vacía."
+	else:
+		return "Esta lista no está vacía."
 
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_list(a, b):
+	if len(a) >= 3 and len(b) >= 3:
+		if a[2] == b[2]:
+			return True
+		else:
+			return False
+	else:
+		return False
 
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(c):
+	if  len(c[0])>=2 and len(c[1])>=4 and len(c[2])>=2:
+		c[0] = c[0][0:2]
+		c[1] = c[1][1:4]
+		c[2] = c[2][-2:]
+		return c
